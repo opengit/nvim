7ecbec7b7e897213b20a630a8192a3e4f1bf2d89
GitOPEN <gitopen@gmail.com>
2024-02-14 14:52:14 +0800

    update nde gui font

--- lua/basic.lua
+++ lua/basic.lua
@@ -24,8 +24,8 @@ vim.opt.synmaxcol = 300
 vim.opt.laststatus = 3
 -- vim.o.guifont = "GoMono Nerd Font:h10:#Bold"
 -- vim.o.guifont = "BlexMono Nerd Font Mono:h10"
-if vim.loop.os_uname().sysname == "OSX" then
-	vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h10"
+if vim.loop.os_uname().sysname == "Darwin" then
+	vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h12"
 else
 	vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h8"
 end

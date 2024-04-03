5cf5627d533dbba94ffcb4f369104af28870fd88
GitOPEN <gitopen@gmail.com>
2024-04-03 16:23:13 +0800

    add Comment.nvim plugin.

--- lua/my_plugins/config.lua
+++ lua/my_plugins/config.lua
@@ -173,4 +173,17 @@ return {
 			vim.keymap.set("n", "<leader>Q", require("confirm-quit").confirm_quit_all)
 		end,
 	},
+	-- --------------------------------------------------------------------------------------------------------------
+	-- make the use of templates.
+	-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
 }

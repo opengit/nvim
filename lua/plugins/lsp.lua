3f8c546b7fd9f5f35146c2ecb300d447a28b1757
GitOPEN <gitopen@gmail.com>
2024-04-06 18:12:38 +0800

    set up the prettier.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,17 +107,30 @@ return {
 			require("mason-null-ls").setup({
 				automatic_setup = true,
 				-- ensure_installed = { "shfmt", "prettier", "stylua", "black", "isort", "flake8" },
+				-- ensure_installed = { "shfmt", "stylua" },
 				ensure_installed = { "shfmt", "prettier", "stylua" },
-				-- ensure_installed = { "shfmt", "prettier", "stylua" },
 				handlers = {
 					-- flake8 = function(source_name, methods)
 					-- 	null_ls.register(null_ls.builtins.diagnostics.flake8.with({
 					-- 		extra_args = { "--max-line-length", "88" },
 					-- 	}))
 					-- end,
+					-- prettier = function(source_name, methods)
+					-- 	null_ls.register(null_ls.builtins.formatting.prettier.with({
+					-- 		extra_args = { "--tab-width", "4" },
+					-- 	}))
+					-- end,
 					prettier = function(source_name, methods)
 						null_ls.register(null_ls.builtins.formatting.prettier.with({
-							extra_args = { "--tab-width", "2" },
+							extra_args = function(params)
+								return params.options
+									and params.options.tabSize
+									and {
+										"--tab-width",
+										"4",
+										-- params.options.tabSize,
+									}
+							end,
 						}))
 					end,
 				},

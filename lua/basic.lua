f21534e0000a7a995ea3ca4e78628562290a425f
GitOPEN <gitopen@gmail.com>
2024-08-15 20:37:42 +0800

    add copy/paste keymaps.

--- lua/basic.lua
+++ lua/basic.lua
@@ -123,6 +123,12 @@ vim.cmd([[
     syntax      on
 ]])
 
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<C-S-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<C-S-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-S-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-S-v>', '<C-R>+', { noremap = true, silent = true })
+
 -- neovide
 if vim.g.neovide then
 	vim.g.neovide_padding_top = 0
@@ -157,4 +163,12 @@ if vim.g.neovide then
 	vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
 	vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
 	vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+
+	-- neovide copy and paste keys
+	vim.keymap.set('n', '<C-S-s>', ':w<CR>')   -- Save
+	vim.keymap.set('v', '<C-S-c>', '"+y')      -- Copy
+	vim.keymap.set('n', '<C-S-v>', '"+P')      -- Paste normal mode
+	vim.keymap.set('v', '<C-S-v>', '"+P')      -- Paste visual mode
+	vim.keymap.set('c', '<C-S-v>', '<C-R>+')   -- Paste command mode
+	vim.keymap.set('i', '<C-S-v>', '<ESC>l"+Pli') -- Paste insert mode
 end

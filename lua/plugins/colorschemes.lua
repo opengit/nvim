7cb0d40620e0e4eebded4feba607e7802f024c5e
GitOPEN <gitopen@gmail.com>
2024-08-28 01:09:12 +0800

    change color

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -43,8 +43,8 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			-- vim.cmd [[colorscheme dracula]]
-			vim.cmd [[colorscheme dracula-soft]]
+			vim.cmd [[colorscheme dracula]]
+			-- vim.cmd [[colorscheme dracula-soft]]
 		end
 	}
 }

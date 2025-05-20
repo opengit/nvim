f13eafad536394239caf78e4d18756d4d79a8804
GitOPEN <gitopen@gmail.com>
2025-05-20 12:01:51 +0800

    Fix: remove codeium plugin.

--- lua/my_plugins/config.lua
+++ lua/my_plugins/config.lua
@@ -60,28 +60,7 @@ return {
 		end,
 	},
 	--------------------------------------------------------------------------------------------------------------
-	-- codeium
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		config = function()
-			-- Change '<C-g>' here to any keycode you like.
-			vim.keymap.set("i", "<C-g>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-;>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-,>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
-			vim.g.codeium_no_map_tab = true
-		end,
-	},
-	-- --------------------------------------------------------------------------------------------------------------
+	--------------------------------------------------------------------------------------------------------------
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",

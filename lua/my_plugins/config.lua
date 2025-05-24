4d1dd4cd5549212a85f52692c617f188d9685647
GitOPEN <gitopen@gmail.com>
2025-05-24 19:47:02 +0800

    Optimize: change the windsurf.nvim to windsurf.vim.

--- lua/my_plugins/config.lua
+++ lua/my_plugins/config.lua
@@ -15,15 +15,17 @@ return {
 		end,
 	},
 	-- --------------------------------------------------------------------------------------------------------------
+	-- Remove the `use` here if you're using folke/lazy.nvim.
 	{
-		"Exafunction/windsurf.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
+		'Exafunction/windsurf.vim',
 		config = function()
-			require("codeium").setup({
-			})
+			-- Change '<C-g>' here to any keycode you like.
+			vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+			vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+				{ expr = true, silent = true })
+			vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+				{ expr = true, silent = true })
+			vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
 		end
 	},
 	--------------------------------------------------------------------------------------------------------------
